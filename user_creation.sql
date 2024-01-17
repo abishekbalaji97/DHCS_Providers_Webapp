ec60d495e45d243130df34f35029c6b1697f7efa
Abishek Balaji <abishek.balaji97@gmail.com>
2024-01-16 20:18:39 -0600

    Delete user_creation.sql

--- user_creation.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'abishekbalaji97'@'localhost' IDENTIFIED BY 'sacredstuff1889%';
-
-GRANT ALL PRIVILEGES ON bloodbankmanagementsystems.* TO 'abishekbalaji97'@'localhost';
-
-GRANT ALL PRIVILEGES ON dhcs_medical_programs.* TO 'abishekbalaji97'@'localhost';
