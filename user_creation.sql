c4b0ba0063d70d47101829133d41b1909bf7b502
Abishek Balaji <abishek.balaji97@gmail.com>
2024-01-16 20:17:56 -0600

    Add files via upload

---
+++ user_creation.sql
@@ -0,0 +1,5 @@
+CREATE USER 'abishekbalaji97'@'localhost' IDENTIFIED BY 'sacredstuff1889%';
+
+GRANT ALL PRIVILEGES ON bloodbankmanagementsystems.* TO 'abishekbalaji97'@'localhost';
+
+GRANT ALL PRIVILEGES ON dhcs_medical_programs.* TO 'abishekbalaji97'@'localhost';
